41e48a7403f985d58d52d5050511d1a57e936810
AmanUpman <amanupman50@gmail.com>
2025-05-12 13:27:58 +0530

    Setting up project and Database

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async() => {
+    try{
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `\n Database Connected !! DB Host : ${
+                connectionInstance.connection.host
+            }`
+        );
+    } catch (error) {
+        console.log("MongoDB connection failed in db/index : ", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Error :" , error);
+    })
+    app.listen(process.env.PORT || 3000, ()=> {
+        console.log(`Server is listening on the port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed in index.js : ", err);
+})
